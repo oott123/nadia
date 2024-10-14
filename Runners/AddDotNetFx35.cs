b0e64948ea5d3ebeb91a7ec128eb26e17069be90
oott123 <git@public.oott123.com>
2024-10-14 17:44:24 +0800

    custom

---
+++ Runners/AddDotNetFx35.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Serilog;
+
+namespace nadia.Runners;
+
+public record class AddDotNetFx35Args
+{
+    public required string Iso { get; init; }
+}
+
+public class AddDotNetFx35 : BaseRunner
+{
+    public override async Task Run(JObject? args)
+    {
+        var ap = args.ToObject<AddDotNetFx35Args>();
+
+        Directory.CreateDirectory("sxs");
+
+        var cab = "sxs\\Microsoft-Windows-NetFx3-OnDemand-Package~31bf3856ad364e35~amd64~~.cab";
+
+        Log.Information("extract ondemand package");
+        await IsoUtils.ExtractFromIso(
+            ap.Iso,
+            "sources\\sxs\\Microsoft-Windows-NetFx3-OnDemand-Package~31bf3856ad364e35~amd64~~.cab",
+            cab
+        );
+
+        Log.Information("apply netfx package");
+        DismUtils.AddPackages(MountDir, [cab]);
+    }
+}
